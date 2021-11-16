97cf2ad364e3d7f3c4acb97e965417fab3ec82aa
Kim Jihoon <max.kim@ebridge-world.com>
2021-11-16 23:06:11 +0900

    settings

--- backend/swe/urls.py
+++ backend/swe/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from swe.views_comments import ViewComment
 from swe.views_movie import ViewMovie, ViewMovieList
+from swe.views_request import ViewRequest
 from swe.views_user import UserSign
 
 urlpatterns = [
@@ -13,6 +14,6 @@ urlpatterns = [
     path('swe/v1/comments', ViewComment.as_view({"post": "create"}), name='comments'),
     path('swe/v1/comments/<comment_id>', ViewComment.as_view({"put": "update"}), name='comments'),
 
-    path('swe/v1/request', ViewComment.as_view({"put": "update"}), name='comments')
+    path('swe/v1/request', ViewRequest.as_view({"post": "create"}), name='comments')
 
 ]
