448d4439c506bea6d50f5669791dec7bad4397e1
Kim <alahoon@naver.com>
2022-09-08 01:28:33 +0900

    feat: genre, movie_genre_assoc views

--- swe/api_urls.py
+++ swe/api_urls.py
@@ -3,9 +3,13 @@ from swe import views as v
 
 urlpatterns = [
     path('movies/', v.MovieList.as_view()),
-    path('movies/<id>/', v.MovieDetail.as_view()),
-    path('movies-images/', v.MovieImageList.as_view()),
-    path('movies-images/<id>/', v.MovieImageDetail.as_view()),
+    path('movies/<int:id>/', v.MovieDetail.as_view()),
+    path('movies/images/', v.MovieImageList.as_view()),
+    path('movies/images/<int:id>/', v.MovieImageDetail.as_view()),
+    path('genres/', v.GenreList.as_view()),
+    path('genres/<int:id>/', v.GenreDetail.as_view()),
+    path('movie-genre-assocs/', v.MovieGenreAssocList.as_view()),
+    path('movie-genre-assocs/<int:id>/', v.MovieGenreAssocDetail.as_view()),
 
     # path('swe/v1/user', UserSign.as_view({"post": "create", "put": "update", "get": "read"}), name='user-sign'),
     # path('swe/v1/user/info', UserInfo.as_view({"get": "read"}), name='user-info'),
--- swe/views.py
+++ swe/views.py
@@ -5,13 +5,22 @@ from rest_framework import filters as rf
 
 from swe import serializer as s
 
+'''
+search_field
+'^' : starts-with search
+'=' : exact matches
+'@' : full-text search
+'$' : regax search
+'''
+
 
 class MovieList(g.ListCreateAPIView):
     queryset = apps.get_model('swe', 'Movie').objects.all()
     serializer_class = s.Movie
-    filter_backends = [rf.OrderingFilter]
+    filter_backends = [rf.OrderingFilter, rf.SearchFilter]
     ordering_field = ['id']
     ordering = ['-id']
+    search_fields = ['title']
 
 
 class MovieDetail(g.RetrieveUpdateDestroyAPIView):
@@ -31,4 +40,32 @@ class MovieImageList(g.ListCreateAPIView):
 class MovieImageDetail(g.RetrieveUpdateDestroyAPIView):
     queryset = apps.get_model('swe', 'MovieImage').objects.all()
     serializer_class = s.MovieImage
+    lookup_field = 'id'
+
+
+class GenreList(g.ListCreateAPIView):
+    queryset = apps.get_model('swe', 'Genre').objects.all()
+    serializer_class = s.Genre
+    filter_backends = [rf.OrderingFilter]
+    ordering_field = ['id']
+    ordering = ['-id']
+
+
+class GenreDetail(g.RetrieveUpdateDestroyAPIView):
+    queryset = apps.get_model('swe', 'Genre').objects.all()
+    serializer_class = s.Genre
+    lookup_field = 'id'
+
+
+class MovieGenreAssocList(g.ListCreateAPIView):
+    queryset = apps.get_model('swe', 'MovieGenreAssoc').objects.all()
+    serializer_class = s.MovieGenreAssoc
+    filter_backends = [rf.OrderingFilter]
+    ordering_field = ['id']
+    ordering = ['-id']
+
+
+class MovieGenreAssocDetail(g.RetrieveUpdateDestroyAPIView):
+    queryset = apps.get_model('swe', 'MovieGenreAssoc').objects.all()
+    serializer_class = s.MovieGenreAssoc
     lookup_field = 'id'
