f8d934fe284a91328d294efc98d9e643fa34959d
Kim <alahoon@naver.com>
2022-09-08 01:28:41 +0900

    feat: genre, movie_genre_assoc tests

--- swe/conftest.py
+++ swe/conftest.py
@@ -7,34 +7,40 @@ from swe import models as m
 
 
 @pytest.fixture
-def code_dummy_A():
-    return m.Code.objects.create(
-        code='dummy code A',
-        name='dummy name A',
-    )
-
-
-@pytest.fixture
-def code_dummy_B():
-    return m.Code.objects.create(
-        code='dummy code B',
-        name='dummy name B',
-    )
-
-
-
-@pytest.fixture
-def movie_dummy():
+def movie_dummy_1():
     return m.Movie.objects.create(
         title='Dummy Movie',
         director='Dummy Director',
         release_date=timezone.now()
     )
 
+@pytest.fixture
+def movie_dummy_2():
+    return m.Movie.objects.create(
+        title='Dummy Movie2',
+        director='Dummy Director2',
+        release_date=timezone.now()
+    )
+
 
 @pytest.fixture
-def movie_image_dummy(movie_dummy):
+def movie_image_dummy(movie_dummy_1):
     return m.MovieImage.objects.create(
-        movie=movie_dummy,
+        movie=movie_dummy_1,
         url='dummy url'
+    )
+
+
+@pytest.fixture
+def genre_dummy():
+    return m.Genre.objects.create(
+        name='dummy genre'
+    )
+
+
+@pytest.fixture
+def movie_genre_assoc_dummy(movie_dummy_1, genre_dummy):
+    return m.MovieGenreAssoc.objects.create(
+        movie=movie_dummy_1,
+        genre=genre_dummy
     )
--- swe/tests.py
+++ swe/tests.py
@@ -1,29 +1,6 @@
 import pytest
-from rest_framework.test import APIClient
-from django.test import TestCase
 
-# Create your tests here.
-from django.urls import reverse
 import json
-
-from swe.models import Code
-
-
-class TestUtil:
-    @pytest.mark.django_db
-    def test_difference_between_update_and_save(self, code_dummy_A, code_dummy_B):
-        before_A_last_update_date = code_dummy_A.last_update_date
-        before_B_last_update_date = code_dummy_B.last_update_date
-
-        # update
-        Code.objects.get(pk=code_dummy_A.pk).save()
-        Code.objects.filter(pk=code_dummy_B.pk).update()
-
-        after_A_last_update_date = Code.objects.get(pk=code_dummy_A.pk).last_update_date
-        after_B_last_update_date = Code.objects.get(pk=code_dummy_B.pk).last_update_date
-
-        assert before_A_last_update_date != after_A_last_update_date
-        assert before_B_last_update_date == after_B_last_update_date
 
 
 class TestViewsMovie:
@@ -34,49 +11,121 @@ class TestViewsMovie:
         assert json.loads(response.content) == []
 
     @pytest.mark.django_db
-    def test_list_should_return_one(self, client, movie_dummy):
+    def test_list_should_return_one(self, client, movie_dummy_1):
         response = client.get('/api/movies/')
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 1
 
-        assert movie_dummy.total_view == 0
-        assert movie_dummy.daily_view == 0
+        assert movie_dummy_1.total_view == 0
+        assert movie_dummy_1.daily_view == 0
 
     @pytest.mark.django_db
-    def test_search_a_movie_should_return_one(self, client, movie_dummy):
-        response = client.get(f'/api/movies/{movie_dummy.pk}/')
+    def test_search_a_movie_should_return_one(self, client, movie_dummy_1):
+        response = client.get(f'/api/movies/{movie_dummy_1.pk}/')
 
         content = json.loads(response.content)
         assert response.status_code == 200
-        assert content['title'] == movie_dummy.title
+        assert content['title'] == movie_dummy_1.title
+
+    @pytest.mark.django_db
+    def test_search_by_movie_ids_should_return_all_matching_movies(self, client, movie_dummy_1, movie_dummy_2):
+        response = client.get(f'/api/movies/?search=movie/')
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        # assert len(response) == 2
+        print(content)
+        # assert content['title'] == movie_dummy_1.title
 
 
 class TestViewsMovieImage:
     @pytest.mark.django_db
     def test_list_should_return_empty_list(self, client):
-        response = client.get('/api/movies-images/')
+        response = client.get('/api/movies/images/')
         assert response.status_code == 200
         assert json.loads(response.content) == []
 
     @pytest.mark.django_db
-    def test_list_should_return_one(self, client, movie_dummy, movie_image_dummy):
-        response = client.get('/api/movies-images/')
+    def test_list_should_return_one(self, client, movie_dummy_1, movie_image_dummy):
+        response = client.get('/api/movies/images/')
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 1
 
-        assert movie_image_dummy.movie == movie_dummy
+        assert movie_image_dummy.movie == movie_dummy_1
 
     @pytest.mark.django_db
     def test_search_a_movie_image_should_return_one(self, client, movie_image_dummy):
-        response = client.get(f'/api/movies-images/{movie_image_dummy.pk}/')
+        response = client.get(f'/api/movies/images/{movie_image_dummy.pk}/')
 
         content = json.loads(response.content)
         assert response.status_code == 200
         assert content['url'] == movie_image_dummy.url
 
     @pytest.mark.django_db
-    def test_list_by_movie(self, client, movie_dummy, movie_image_dummy):
-        response = client.get(f'/api/movies-images/?movie={movie_dummy.pk}/')
+    def test_list_by_movie(self, client, movie_dummy_1, movie_image_dummy):
+        response = client.get(f'/api/movies/images/?movie={movie_dummy_1.pk}/')
+
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        assert 1 == len(content)
+
+
+class TestViewsGenre:
+    @pytest.mark.django_db
+    def test_list_should_return_empty_list(self, client):
+        response = client.get('/api/genres/')
+        assert response.status_code == 200
+        assert json.loads(response.content) == []
+
+    @pytest.mark.django_db
+    def test_list_should_return_one(self, client, genre_dummy):
+        response = client.get('/api/genres/')
+        assert response.status_code == 200
+        content = json.loads(response.content)
+        assert len(content) == 1
+        assert genre_dummy.name == content[0]['name']
+
+    @pytest.mark.django_db
+    def test_search_a_movie_image_should_return_one(self, client, genre_dummy):
+        response = client.get(f'/api/genres/{genre_dummy.pk}/')
+
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        assert content['name'] == genre_dummy.name
+
+
+class TestViewsMovieGenreAssoc:
+    @pytest.mark.django_db
+    def test_list_should_return_empty_list(self, client):
+        response = client.get('/api/movie-genre-assocs/')
+        assert response.status_code == 200
+        assert json.loads(response.content) == []
+
+    @pytest.mark.django_db
+    def test_list_should_return_one(self, client, movie_genre_assoc_dummy):
+        response = client.get('/api/movie-genre-assocs/')
+        assert response.status_code == 200
+        content = json.loads(response.content)
+        assert len(content) == 1
+
+    @pytest.mark.django_db
+    def test_search_a_movie_image_should_return_one(self, client, movie_genre_assoc_dummy):
+        response = client.get(f'/api/movie-genre-assocs/{movie_genre_assoc_dummy.pk}/')
+
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        print('content:', content)
+
+    @pytest.mark.django_db
+    def test_list_by_movie(self, client, movie_dummy_1, movie_genre_assoc_dummy):
+        response = client.get(f'/api/movie-genre-assocs/?movie={movie_dummy_1.pk}/')
+
+        content = json.loads(response.content)
+        assert response.status_code == 200
+        assert 1 == len(content)
+
+    @pytest.mark.django_db
+    def test_list_by_genre(self, client, genre_dummy, movie_genre_assoc_dummy):
+        response = client.get(f'/api/movie-genre-assocs/?genre={genre_dummy.pk}/')
 
         content = json.loads(response.content)
         assert response.status_code == 200
